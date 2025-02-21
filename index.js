1d0d27a99fea8de3ae4c2a278f66e8de34460b89
CompSciHigh24 <csh-github@compscihigh.org>
2025-02-21 12:01:56 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// Step 0: Make sure you have Express installed (`npm install express`).
+
+// Step 1: Import the Express module
+
+// Step 2: Create an instance of an Express application
+
+// Step 3: Start the server on port 3000
+
+// Step 4: Run this file, use the command: `node index.js`
